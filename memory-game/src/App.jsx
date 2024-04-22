f44e946366eb622a3ca180a2b7f8c0586244cf45
alfaindomart <alfaindomart23@gmail.com>
2024-04-22 21:51:25 +0700

    fetch, slice, and render the imported data

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,34 +1,46 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
+// import {nanoid} from 'nanoid'
+
 import './App.css'
 
 function App() {
 
   const [digimon, setDigimon] = useState([])
-  // const [score, setScore] = useState(0)
+  const [score, setScore] = useState(0)
 
   useEffect(() => {
     fetch('https://digimon-api.vercel.app/api/digimon')
-      .then((response) => response.json())
-      .then ((data) => {
-        const filteredMons = data.slice(0, 10)
-        setDigimon(filteredMons)})
-      .catch((error) => {
-        return console.error(error)
-      })
+    .then((response) => response.json())
+    .then ((data) => {
+      const filteredMons = data.slice(0, 10)
+      setDigimon(...digimon, filteredMons)
+    })
+    .catch((error) => {
+      return console.error(error)
+    })
   }, [])
 
-  const digiSliced = digimon.map(mons => 
-    <img src={mons.img} alt={'A picture of ' + mons.name} key={mons.id} onClick={console.log('test klik gambar')} />
+  console.log(digimon)
+
+  const digiSliced = digimon.map(
+    (mons, index) => <img src={mons.img} alt={'test'} key={index} onClick={Testbtn}/>
     )
 
+    console.log(digiSliced)
+
+
+function Testbtn() {
+  setScore(score + 1)
+}
 
   return (
 
     <>
     <div className='score-wrap'>
-      <div className='curr-score'>Current Score: </div>
-      <div className="high-score">High Score: </div>
+      <button onClick={Testbtn}>click</button>
+      <div className='curr-score'>Current Score: {score}</div>
+      <div className="high-score">High Score: {score}</div>
     </div>
     <div className='cards-wrapper'>
       {digiSliced}
